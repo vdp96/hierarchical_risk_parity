9c99df1eacc98f0280bcae4e3dbf9d1a20d46590
vdp96 <vpunjala1996@gmail.com>
2023-12-23 13:30:37 -0800

    commit 2: added allocation library

---
+++ allocation.py
@@ -0,0 +1,89 @@
+import pandas as pd
+import numpy as np
+import hrp
+import utils as hrp_utils
+
+
+def get_hrp_weights(df: pd.DataFrame, linkage_type: str = "single", distance_type: str = "euclidean") -> pd.Series:
+    """
+    Computes HRP allocation for a portfolio according to initial paper. Has additional linkage types to explore
+
+    :param df: dataframe with assets, returns and date
+    :param linkage_type: [single, complete, ward, average]
+    :param distance_type: [euclidean,
+    :return:
+    """
+
+    # correlation and covariance matrix
+    correlation_matrix = hrp_utils.create_correlation_matrix(df=df, corr_type="spearman")
+    covariance_matrix = hrp_utils.create_correlation_matrix(df=df, covariance=True)
+
+    # linkage matrix
+    linkage_matrix = hrp.get_linkage(corr_matrix=correlation_matrix, linkage_type=linkage_type,
+                                     distance_type=distance_type)
+
+    # quasi diagonalization
+    qd_idx = hrp.perform_quasi_diagonalization(linkage_matrix=linkage_matrix)
+
+    # weights from recursive bipartition
+    weights = hrp.perform_recursive_bipartition(covariance_matrix=covariance_matrix, quasi_diag_idx=qd_idx)
+
+    # creating output series with tickers and weights
+    tickers = [covariance_matrix.columns[i] for i in qd_idx]
+    return pd.Series(weights, index=tickers, name="HRP")
+
+
+def get_mv_weights(df: pd.DataFrame) -> pd.Series:
+    """
+    Computes weights based on minimum variance allocation
+    :param df: dataframe with assets, returns and date
+    :return: Series with tickers and weights
+    """
+    # covariance matrix
+    covariance_matrix = hrp_utils.create_correlation_matrix(df=df, covariance=True)
+    tickers = covariance_matrix.columns
+
+    # inverse cov matrix
+    inv_covar = np.linalg.inv(covariance_matrix)
+
+    # calculating min var allocation
+    u = np.ones(len(covariance_matrix))
+    weights = np.dot(inv_covar, u) / np.dot(u, np.dot(inv_covar, u))
+    return pd.Series(weights, index=tickers, name="MV")
+
+
+def get_rp_weights(df: pd.DataFrame) -> pd.Series:
+    """
+    Computes Risk parity allocation
+
+    :param df: dataframe with assets, returns and date
+    :return: Series with tickers and weights
+    """
+    # covariance matrix
+    covariance_matrix = hrp_utils.create_correlation_matrix(df=df, covariance=True)
+    tickers = covariance_matrix.columns
+
+    # risk parity allocation
+    weights = (1 / np.diag(covariance_matrix))
+    weights /= sum(weights)
+    return pd.Series(weights, index=tickers, name="RP")
+
+
+def get_unif_weights(df: pd.DataFrame) -> pd.Series:
+    """
+    Computes equal weighted returns
+
+    :param df: dataframe with assets, returns and date
+    :return: Series with tickers and weights
+    """
+    # tickers
+    tickers = df.ticker.unique()
+
+    # weights
+    weights = [1 / len(tickers)] * len(tickers)
+    return pd.Series(weights, index=tickers, name="unif")
+
+
+def compute_ER(returns, weights):
+    mean = returns.mean(0)
+    return weights.values * mean
--- hrp.py
+++ hrp.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import hrp_utils
+import utils as hrp_utils
 import os
 from scipy.cluster.hierarchy import linkage, leaves_list
 
@@ -14,11 +14,14 @@ def get_linkage(corr_matrix: np.array, linkage_type: str, distance_type: str) ->
     This function gets linkage matrix using scipy hierarchy library
 
     :param corr_matrix: correlation matrix for the universe
-    :param linkage_type: ["single", "ward", "complete", "average"
+    :param linkage_type: ["single", "ward", "complete", "average"]
     :param distance_type:
     :return: linkage matrix
     """
+    # distance
     corr_distances = np.sqrt(0.5 * (1 - corr_matrix))
+
+    # linkage
     linkage_matrix = linkage(y=corr_distances, method=linkage_type, metric=distance_type)
     return linkage_matrix
 
@@ -97,50 +100,6 @@ def get_cluster_variance(covariance_matrix: pd.DataFrame, cluster_idx: np.array)
     return cluster_var
 
 
-def compute_HRP_weights(df: pd.DataFrame, linkage_type: str = "single", distance_type: str = "euclidean"):
-    """
-    Computes HRP allocation for a portfolio according to initial paper. Has additional linkage types to explore
-
-    :param df: dataframe with assets, returns and date
-    :param linkage_type: [single, complete, ward, average]
-    :param distance_type: [euclidean,
-    :return:
-    """
-    correlation_matrix = hrp_utils.create_correlation_matrix(df=df, corr_type="spearman")
-    covariance_matrix = hrp_utils.create_correlation_matrix(df=df, covariance=True)
-
-    linkage_matrix = get_linkage(corr_matrix=correlation_matrix, linkage_type=linkage_type, distance_type=distance_type)
-
-    qd_idxes = perform_quasi_diagonalization(linkage_matrix=linkage_matrix)
-
-    weights = perform_recursive_bipartition(covariance_matrix=covariance_matrix, quasi_diag_idx=qd_idxes)
-
-    return weights
-
-
-def compute_MV_weights(covariances, tickers):
-    inv_covar = np.linalg.inv(covariances)
-    u = np.ones(len(covariances))
-    x = np.dot(inv_covar, u) / np.dot(u, np.dot(inv_covar, u))
-    return pd.Series(x, index=tickers, name="MV")
-
-
-def compute_RP_weights(covariances, tickers):
-    weights = (1 / np.diag(covariances))
-    x = weights / sum(weights)
-    return pd.Series(x, index=tickers, name="RP")
-
-
-def compute_unif_weights(covariances, tickers):
-    x = [1 / len(covariances) for i in range(len(covariances))]
-    return pd.Series(x, index=tickers, name="unif")
-
-
-def compute_ER(returns, weights):
-    mean = returns.mean(0)
-    return weights.values * mean
-
-
 def do():
     ticker_name_map = hrp_utils.get_snp_constituents_for_date(date="2020-12-31", wrds_id="vpunjala1996")
     snp_tickers = list(ticker_name_map.keys())
@@ -149,8 +108,7 @@ def do():
 
     data = hrp_utils.clean_dataset(df=data, fillna=True)
     corr = hrp_utils.create_correlation_matrix(df=data)
-    corr_dist = np.sqrt(0.5 * (1 - corr))
-    link = get_linkage(corr_distances=corr_dist, linkage_type="ward", distance_type="euclidean")
+    link = get_linkage(corr_matrix=corr, linkage_type="ward", distance_type="euclidean")
     x = perform_quasi_diagonalization(link)
     cov = hrp_utils.create_correlation_matrix(data, covariance=True)
 
@@ -158,5 +116,4 @@ def do():
 
     return w
 
-
-print(do())
+# print(do())
